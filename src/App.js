60eeee8f7fc62faacb9c7971ad8655f362a01daa
yanyubin <yanyubin@autohome.com.cn>
2018-02-21 10:50:00 +0800

    modify

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import './App.css';
+import { Route } from 'react-router-dom'
+import HomePage from './components/HomePage'
+import LoginPage from './components/LoginPage'
 
-const App = () => <div>Hello World!</div>
+const App = () => (<div>
+	<Route path="/" exact component={HomePage} />
+	<Route path="/login" exact component={LoginPage} />
+</div>)
 
 export default App;
---
+++ src/components/LoginPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const LoginPage = () => (
+    <div>
+		<h1>Login Page</h1>
+	</div>
+)
+
+export default LoginPage
