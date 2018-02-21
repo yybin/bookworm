3f8d38511281773011571da8b25b558cb6ff9dd2
yanyubin <yanyubin@autohome.com.cn>
2018-02-21 10:59:42 +0800

    update

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { Route } from 'react-router-dom'
 import HomePage from './components/HomePage'
 import LoginPage from './components/LoginPage'
 
-const App = () => (<div>
-	<Route path="/" exact component={HomePage} />
-	<Route path="/login" exact component={LoginPage} />
-</div>)
+const App = () => (
+    <div className="ui container">
+		<Route path="/" exact component={HomePage} />
+		<Route path="/login" exact component={LoginPage} />
+	</div>
+)
 
 export default App;
